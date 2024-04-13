e0c2d1eb8ee04f7961a3a59ad545b19ff198a1ee
berlovesumwah <166808321+weberbers201938@users.noreply.github.com>
2024-04-13 11:34:33 +0800

    Update autoacpm.js

--- script/event/autoacpm.js
+++ script/event/autoacpm.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra');
-const pathFile = __dirname + '/txt/autoacpm.txt';
+const path = require('path'); // Add path package
+const pathFile = path.join(__dirname, 'txt', 'autoacpm.txt'); // Using path.join() for file paths
 
 module.exports.config = {
   name: "autoacpm",
@@ -7,28 +8,28 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = async ({ api, event, args, admin, prefix, botName}) => {
-let timestamp;
-const nowTime = Date.now();
-let callbackMS
-if (!fs.existsSync(pathFile))
-   fs.writeFileSync(pathFile, 'false');
-   const isEnable = fs.readFileSync(pathFile, 'utf-8');
-   if (isEnable == 'true') {
-      const list = [ 
-        ...(await api.getThreadList(1, null, ['PENDING'])), 
-        ...(await api.getThreadList(1, null, ['OTHER'])) 
-      ];
-          if (list[0]) {
-api.changeNickname(`${prefix} | ${botName}`, list[0].threadID, api.getCurrentUserID());
-         api.sendMessage("Connecting...", list[0].threadID, (err, info) => {
-                timestamp = info.timestamp;
-                callbackMS = Date.now();
-              });
-              await new Promise(resolve => setTimeout(resolve, 3000));
-              const latency = timestamp - nowTime;
-              const callbackTime = callbackMS - nowTime;
-   
+  let timestamp;
+  const nowTime = Date.now();
+  let callbackMS;
+  if (!fs.existsSync(pathFile))
+    fs.writeFileSync(pathFile, 'false');
+  const isEnable = fs.readFileSync(pathFile, 'utf-8');
+  if (isEnable == 'true') {
+    const list = [ 
+      ...(await api.getThreadList(1, null, ['PENDING'])), 
+      ...(await api.getThreadList(1, null, ['OTHER'])) 
+    ];
+    if (list[0]) {
+      api.changeNickname(`${prefix} | ${botName}`, list[0].threadID, api.getCurrentUserID());
+      api.sendMessage("Connecting...", list[0].threadID, (err, info) => {
+        timestamp = info.timestamp;
+        callbackMS = Date.now();
+      });
+      await new Promise(resolve => setTimeout(resolve, 3000));
+      const latency = timestamp - nowTime;
+      const callbackTime = callbackMS - nowTime;
+
       await api.sendMessage(`🌟 This thread is automatically approved by our system, Enjoy!\n\n╭───❒Accepting Thread Connection:\n│─ Status: Online\n│─ Botname: ${botName}\n│─ Owner:\n│─https://facebook.com/${admin[0]}\n│─ Prefix: ${prefix}\n╰───────────𖤓\n╭───❒Checking Ping:\n│─ Latency: Input = ${latency} ms\n│─ Callback = ${callbackTime} ms\n│─ Input & Callback Difference: ${callbackTime - latency} ms\n│─ Use ${prefix}help to view command details\n╰───────────𖤓`, list[0].threadID);
-          }
     }
+  }
 };
