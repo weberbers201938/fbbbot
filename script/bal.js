76bab04d61173f837185a8273d35d7ecb49e4a96
berlovesumwah <166808321+weberbers201938@users.noreply.github.com>
2024-04-23 18:07:28 +0800

    Update bal.js

--- script/bal.js
+++ script/bal.js
@@ -9,14 +9,29 @@ module.exports.config = {
   usage: "{pref}[name of cmd]",
   credits: "Ainz"
 };
-module.exports.run = async function({ api, event, args, Currencies }) {
-  const { threadID, messageID, senderID } = event;
-  const { getData, increaseMoney, decreaseMoney } = Currencies;
+
+module.exports.run = async function({ api, event, args, Currencies, Utils }) {
+  const { getData } = Currencies;
+  const { senderID, threadID } = event;
+
   const money = await getData(senderID);
   const moneyBalUser = money.money;
   const nameOfUser = money.name;
-  api.sendMessage(
-    `Hi ${nameOfUser}🌠\nYour balance is ${moneyBalUser}✨`,
-    threadID
-  );
+ 
+  const i = await api.sendMessage('🌀 Loading.', threadID);
+  
+  await Utils.delay(1000);
+  api.editMessage(`🌠 Hi ${nameOfUser}`, i.messageID);
+
+  await Utils.delay(1000);
+  api.editMessage(`🌠 Hi ${nameOfUser} Yo`, i.messageID);
+
+  await Utils.delay(1000);
+  api.editMessage(`🌠 Hi ${nameOfUser} Your bala`, i.messageID);
+  
+  await Utils.delay(1000);
+  api.editMessage(`🌠 Hi ${nameOfUser} Your balance is`, i.messageID);
+  
+  await Utils.delay(1000);
+  api.editMessage(`🌠 Hi ${nameOfUser} Your balance is ${moneyBalUser}`, i.messageID);
 };
